1023df5ed037f8333a58e1e32161883aba77e3c6
Matty Jones <urlugal@gmail.com>
2020-09-07 22:23:37 -0400

    update secrets to be unique
    Signed-off-by: Matty Jones <urlugal@gmail.com>

--- langs/mock.go
+++ langs/mock.go
@@ -2,86 +2,86 @@ package main
 
 func main() {
 
-	secret_key := "foobarbaz"
-	customer_key := "foobarbaz"
-	consumer_key := "foobarbaz"
+	secret_key := "foobarbaz-g1"
+	customer_key := "foobarbaz-g2"
+	consumer_key := "foobarba-g3"
 
-	secretKey := "foobarbaz"
-	customerKey := "foobarbaz"
-	consumerKey := "foobarbaz"
+	secretKey := "foobarbaz-g4"
+	customerKey := "foobarbaz-g5"
+	consumerKey := "foobarbaz-g6"
 
-	var secret_key4 = "foobarbaz"
-	var customer_key4 = "foobarbaz"
-	var consumer_key4 = "foobarbaz"
+	var secret_key4 = "foobarbaz-g7"
+	var customer_key4 = "foobarbaz-g8"
+	var consumer_key4 = "foobarbaz-g9"
 
 	var secret_key2 string
 	var customer_key2 string
 	var consumer_key2 string
 
-	secretKey = "foobarbaz"
-	customer_key = "foobarbaz"
-	consumerkey := "foobarbaz"
+	secretKey = "foobarbaz-g10"
+	customer_key = "foobarbaz-g11"
+	consumerkey := "foobarbaz-g12"
 
-	secret_key = "foobarbaz"
-	customer_key = "foobarbaz"
-	consumer_key = "foobarbaz"
+	secret_key = "foobarbaz-g13"
+	customer_key = "foobarbaz-g14"
+	consumer_key = "foobarbaz-g15"
 
-	pwd3 := "foobarbaz"
-	var pwd = "foobarbaz"
-	pwd2 := "foobarbaz"
+	pwd3 := "foobarbaz-g16"
+	var pwd = "foobarbaz-g17"
+	pwd2 := "foobarbaz-g18"
 
-	password := "foobarbaz"
-	nexuspassword := "foobarbaz"
-	pass := "foobarbaz"
+	password := "foobarbaz-g19"
+	nexuspassword := "foobarbaz-g20"
+	pass := "foobarbaz-g21"
 
-	var pass5 = "foobarbaz"
+	var pass5 = "foobarbaz-g22"
 
-	password = "foobarbaz"
-	nexuspassword = "foobarbaz"
-	pass = "foobarbaz"
+	password = "foobarbaz-g23"
+	nexuspassword = "foobarbaz-g24"
+	pass = "foobarbaz-g25"
 
-	passwd := "foobarbaz"
-	var passwd5 = "foo"
+	passwd := "foobarbaz-g26"
+	var passwd5 = "foo-g27"
 	passwd2 := "foo@#$%*__----+=Baz"
 
-	cred := "foobarbaz"
-	creds := "foobarbaz"
-	credentials := "foobarbaz"
-	credential := "foobarbaz"
+	cred := "foobarbaz-g28"
+	creds := "foobarbaz-g29"
+	credentials := "foobarbaz-g31"
+	credential := "foobarbaz-g32"
 
-	var cred7 = "foobarbaz"
-	var creds0 = "foobarbaz"
-	var credentials2 = "foobarbaz"
-	var credential1 = "foobarbaz"
+	var cred7 = "foobarbaz-g33"
+	var creds0 = "foobarbaz-g34"
+	var credentials2 = "foobarbaz-g35"
+	var credential1 = "foobarbaz-g36"
 
-	cred = "foobarbaz"
-	creds = "foobarbaz"
-	credentials = "foobarbaz"
-	credential = "foobarbaz"
+	cred = "foobarbaz-g37"
+	creds = "foobarbaz-g38"
+	credentials = "foobarbaz-g39"
+	credential = "foobarbaz-g40"
 
-	auth := "foobarbaz"
-	authorization := "foobarbaz"
+	auth := "foobarbaz-g41"
+	authorization := "foobarbaz-g42"
 
-	var auth98 = "foobarbaz"
-	var authorization4 = "foobarbaz"
+	var auth98 = "foobarbaz-g43"
+	var authorization4 = "foobarbaz-g44"
 
-	auth = "foobarbaz"
-	authorization = "foobarbaz"
+	auth = "foobarbaz-g45"
+	authorization = "foobarbaz-g46"
 
 }
 
-func keyAuth("AKIAIGTOPSI4SXYCHGHQ"){
-    access_key := "AKIAIGTOPSI4SXYCHGHQ"
-    var access_key = "AKIAIGTOPSI4SXYCHGHQ"
-    access_key = "AKIAIGTOPSI4SXYCHGHQ"
-    m["access_key"] = "AKIAIGTOPSI4SXYCHGHQ"
-    if access_key == 'AKIAIGTOPSI4SXYCHGHQ' {
+func keyAuth("AKIAIGTOPSI4SXYCHGH0"){
+    access_key := "AKIAIGTOPSI4SXYCHGH1"
+    var access_key = "AKIAIGTOPSI4SXYCHGH2"
+    access_key = "AKIAIGTOPSI4SXYCHGH3"
+    m["access_key"] = "AKIAIGTOPSI4SXYCHGH4"
+    if access_key == 'AKIAIGTOPSI4SXYCHGH5' {
         return true
     }
-    access_key := [2]string{"AKIAIGTOPSI4SXYCHGHQ", "z9345456dfgvssfnyg"}
+    access_key := [2]string{"AKIAIGTOPSI4SXYCHGH6", "z9345456dfgvssfnyg"}
     keys := map[string]int{
-        "key1": "AKIAIGTOPSI4SXYCHGHQ",
-        "key2": "AKIAIJVQDVKHVTFUNLPN",
-        "key3": "AKIAIBOOPSI4SXAHWGHQ",
+        "key1": "AKIAIGTOPSI4SXYCHGH7",
+        "key2": "AKIAIJVQDVKHVTFUNLP9",
+        "key3": "AKIAIBOOPSI4SXAHWGH8",
     }
 }
--- langs/mock.java
+++ langs/mock.java
@@ -1,28 +1,28 @@
 //
 // secret-key
 //
-String secret-key = "Hello world!";
-String customer-key = "Hello World!";
-String consumer-key = "Hello World!";
+String secret-key = "Hello worldj1!";
+String customer-key = "Hello Worldj2!";
+String consumer-key = "Hello World!j3";
 
-String secret_key = "Hello world!";
-String customer_key = "Hello World!";
-String consumer_key = "Hello World!";
+String secret_key = "Hello world!j4";
+String customer_key = "Hello World!j5";
+String consumer_key = "Hello World!j6";
 
-String pwd = "Hello world!";
+String pwd = "Hello world!j7";
 
-String password = "Hello world!";
-String nexuspassword = "Hello world!";
-String pass = "Hello world!";
+String password = "Hello world!j8";
+String nexuspassword = "Hello world!j9";
+String pass = "Hello world!j0";
 
-String passwd = "Hello world!";
+String passwd = "Hello world!j11";
 
-String cred = "Hello world!";
-String creds = "Hello world!";
-String credentials = "Hello world!";
-String credential = "Hello world!";
+String cred = "Hello world!j12";
+String creds = "Hello world!j13";
+String credentials = "Hello world!j14";
+String credential = "Hello world!j15";
 
-String access_key = "AKIAIGTOPSI4SXYCHGHQ";
+String access_key = "AKIAIGTOPSI4SXYCHG16";
 
-String auth = "Hello world!";
-String authorization = "Hello world!";
+String auth = "Hello world!j18";
+String authorization = "Hello world!j19";
